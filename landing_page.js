c1f08d1d410351aef2e7c53f13abd96dbf2fa657
ven <margsmay23@gmail.com>
2025-03-11 13:36:49 +0800

    - FIxed

---
+++ landing_page.js
@@ -0,0 +1,45 @@
+// Update the Date daily
+function updateDate() {
+    const dateElement = document.querySelector('.date');
+    const today = new Date();
+    
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = today.toLocaleDateString('en-US', options);
+    
+    dateElement.textContent = formattedDate;
+  }
+    
+  updateDate();
+  // End of Date Function
+    
+    
+  // Image Slider Section
+  let slideIndex = 1;
+  showSlides(slideIndex);
+    
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+    
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+    
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+// End of Image Slider Section
