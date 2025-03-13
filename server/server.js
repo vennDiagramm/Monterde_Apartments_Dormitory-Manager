5e182292d53ea6611ca8a0867fce572d1b92d6bf
ven <margsmay23@gmail.com>
2025-03-13 12:04:55 +0800

    Added GET for Room tables
    ### TO DO:
    - Put the tables in (Reports, Dismiss)

--- server/server.js
+++ server/server.js
@@ -257,6 +257,18 @@ app.post('/add-person', async (req, res) => {
 });
 
 
+// Dismiss tenant table
+app.get('/viewTenantInfo', async (req, res) => {
+    try {
+        const [rows] = await db.query('GetTenantInformation()');
+        res.json(rows[0]); // Note: Results are in the first element of the returned array
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Database error" });
+    }
+});
+
+
 // Remove Tenant Route
 app.delete('/remove-tenant/:personId', async (req, res) => {
     const connection = await db.getConnection();
