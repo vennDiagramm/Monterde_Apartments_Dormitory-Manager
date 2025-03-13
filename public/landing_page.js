458dee4705e2e88e53583ca5e48faa1695e9cb05
Darwin Guirigay <darwinlouis4@gmail.com>
2025-03-13 12:45:47 +0800

    Fixed Responsiveness of Landing Page

--- public/landing_page.js
+++ public/landing_page.js
@@ -13,33 +13,53 @@ function updateDate() {
   // End of Date Function
     
     
-  // Image Slider Section
-  let slideIndex = 1;
-  showSlides(slideIndex);
+//   // Image Slider Section
+//   let slideIndex = 1;
+//   showSlides(slideIndex);
     
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//   // Next/previous controls
+//   function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
     
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   // Thumbnail image controls
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
     
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+//   function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+//     let dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+//   }
+// // End of Image Slider Section
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  function changeSlide() {
+      // Remove 'active' from current slide
+      slides[currentSlide].classList.remove("active");
+
+      // Move to the next slide or loop back to the first
+      currentSlide = (currentSlide + 1) % slides.length;
+
+      // Add 'active' to new slide
+      slides[currentSlide].classList.add("active");
   }
-// End of Image Slider Section
+
+  // Change slide every 5 seconds
+  setInterval(changeSlide, 5000);
+});
